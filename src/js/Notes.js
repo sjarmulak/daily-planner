d894b6b185049a1f2146442dd902b81cb219bea2
sjarmulak <sarahjarmulak@gmail.com>
2022-04-04 15:55:35 +0200

    functional notepad widget

--- src/js/Notes.js
+++ src/js/Notes.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../scss/Notes.scss";
 
 export default function Notes() {
+  const [myNotes, setMyNotes] = useState("");
+
+  const saveNotes = (e) => {
+    setMyNotes(e.target.value);
+    localStorage.setItem("notes", e.target.value);
+  };
+
   return (
     <div className="Notes">
       <div className="notepad">
         <span>my notes:</span>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          value={localStorage.notes}
+          onKeyPress={saveNotes}
+          onChange={saveNotes}
+        ></textarea>
       </div>
     </div>
   );
