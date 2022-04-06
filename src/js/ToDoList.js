2b83cdb8cfeb4298f7112faf48a932ed911d1c23
sjarmulak <sarahjarmulak@gmail.com>
2022-04-06 23:46:26 +0200

    todolist cleanup

--- src/js/ToDoList.js
+++ src/js/ToDoList.js
@@ -68,7 +68,11 @@ export default function ToDoList() {
       </ul>
 
       <ul className="done">
-        <span>done:</span>
+        {todoList
+          ? todoList.filter((t) => t.finished === true).length > 0 && (
+              <span>done:</span>
+            )
+          : null}
         {todoList ? (
           todoList.map(
             ({ id, task, finished }) =>
@@ -90,142 +94,3 @@ export default function ToDoList() {
     </div>
   );
 }
-
-// POPRZEDNIA WERSJA //
-
-// import React, { useState, useEffect } from "react";
-// import ToDoTask from "./ToDoTask";
-// import AddNewTask from "./AddNewTask";
-// import "../scss/ToDoList.scss";
-
-// const url = "http://localhost:3000/todos";
-
-// export default function ToDoList() {
-//   const [todoList, setTodoList] = useState(false);
-//   const [toggledTask, setToggledTask] = useState({}); // biezace zadanie ktore zmieniamy na zrobione/niezrobione
-//   const [currID, setCurrID] = useState(null);
-
-//   // ściągamy naszą listę todo
-//   const getTodos = () => {
-//     fetch(url)
-//       .then((resp) => {
-//         if (resp.ok) {
-//           return resp.json();
-//         }
-//         throw new Error("error");
-//       })
-//       .then((data) => {
-//         setTodoList(data);
-//         console.log("Update");
-//       })
-//       .catch((err) => {
-//         throw new Error(err);
-//       });
-//   };
-
-//   // uaktualniamy listę todo przy renderze
-//   useEffect(getTodos, []);
-
-//   // co się dzieje po "odhaczeniu" zadania
-//   const handleTaskDone = (id) => {
-//     fetch(url + `/${id}`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setToggledTask({ finished: !data.finished }); // zmieniamy zadanie na zrobione/niezrobione
-//         setCurrID(id);
-//         getTodos();
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//     getTodos();
-//     fetch(url + `/${currID}`, {
-//       method: "PATCH", // uaktualniamy w db
-//       body: JSON.stringify(toggledTask),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((resp) => {
-//         if (resp.ok) {
-//           return resp.json();
-//         } else throw new Error("Oops...");
-//       })
-//       .then((data) => {
-//         console.log(data);
-//         getTodos();
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-
-//   // usuwamy całkowicie zadanie z listy
-//   const handleTaskDeleted = (id) => {
-//     fetch(url + `/${id}`, {
-//       method: "DELETE",
-//     })
-//       .then((resp) => {
-//         if (resp.ok) {
-//           console.log("Todo deleted");
-//           getTodos();
-//         } else throw new Error("Oops...");
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//     getTodos();
-//   };
-
-//   // po dodaniu nowego zadania uaktualniamy listę
-//   const handleTaskAdded = () => {
-//     getTodos();
-//   };
-
-//   return (
-//     <div className="ToDoList">
-//       <ul className="notYetDone">
-//         <span>to do:</span>
-//         {todoList ? (
-//           todoList.map(
-//             ({ id, task, finished }) =>
-//               !finished && (
-//                 <ToDoTask
-//                   key={id}
-//                   id={id}
-//                   task={task}
-//                   finished={finished}
-//                   onDone={handleTaskDone}
-//                   onDelete={handleTaskDeleted}
-//                 />
-//               )
-//           )
-//         ) : (
-//           <p>loading...</p>
-//         )}
-//         <AddNewTask onAdded={handleTaskAdded} />
-//       </ul>
-
-//       <ul className="done">
-//         <span>done:</span>
-//         {todoList ? (
-//           todoList.map(
-//             ({ id, task, finished }) =>
-//               finished && (
-//                 <ToDoTask
-//                   key={id}
-//                   id={id}
-//                   task={task}
-//                   finished={finished}
-//                   onDone={handleTaskDone}
-//                   onDelete={handleTaskDeleted}
-//                 />
-//               )
-//           )
-//         ) : (
-//           <p>loading...</p>
-//         )}
-//       </ul>
-//     </div>
-//   );
-// }
